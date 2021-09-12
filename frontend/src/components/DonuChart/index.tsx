32301f419945149d24efde203ce686c81377f2d3
Carlos-Henrique98 <ichfs9108@gmail.com>
2021-09-12 01:07:17 -0300

    Barchart integration

--- frontend/src/components/DonuChart/index.tsx
+++ frontend/src/components/DonuChart/index.tsx
@@ -16,7 +16,6 @@ const DonuChart = () => {
         series: []
     })
 
-
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
@@ -27,14 +26,6 @@ const DonuChart = () => {
                 setChartData({ labels: myLabels, series: mySeries })
             });
     }, []);
-
-
-
-
-    //const mockData = {
-    //  series: [477138, 499928, 444867, 220426, 473088],
-    // labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
